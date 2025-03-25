a0d9352622d73ed6bdffbd2806bfb76b1bd0c6c6
nelsonlopez500 <nelson.lopez@catolica.edu.sv>
2025-03-24 22:00:36 -0600

    Mantenimiento de Equipos

---
+++ FormulariosConRazorYHTML/Controllers/EquiposController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FormulariosConRazorYHTML.Controllers
+{
+    public class EquiposController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
